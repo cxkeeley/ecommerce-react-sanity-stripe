8830d65f55ab91f26bd9202f2cd48aa73fb0aab6
cxkeeley <cxkeeley@gmail.com>
2022-05-20 21:53:52 +0700

    commit

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+        HeroBanner
+
+        <div className='products-heading'>
+            <h2>Best Selling Products</h2>
+            <p>Speakers of many variations</p>
+        </div>
+
+        <div className="products-container">
+            {['Product 1', 'Product 2'].map((product) => product)}
+        </div>
+
+        Footer
+    </>
   )
 }
 
-export default Home;
+export default Home; 
