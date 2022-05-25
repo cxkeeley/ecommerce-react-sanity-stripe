d9502bdb9a55705619b2bbe2b859c95298849d4b
cxkeeley <cxkeeley@gmail.com>
2022-05-25 20:54:38 +0700

    commit

--- ecommerce/lib/client.js
+++ ecommerce/lib/client.js
@@ -1,10 +1,15 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-const client = sanityClient({
-    projectID: '',
-    dataset: '',
-    apiVersion: '',
-    useCdn: true,
-    token: ''
-})
+export const client = sanityClient({
+	projectId: 'faz7btnt',
+	dataset: 'production',
+	apiVersion: '2022-03-10',
+	useCdn: true,
+	token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+});
+
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = source => builder.image(source);
