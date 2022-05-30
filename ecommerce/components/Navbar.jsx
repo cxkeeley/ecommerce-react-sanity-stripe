50783fb54923dba36b129a38f0ff27e96d2bfa85
cxkeeley <cxkeeley@gmail.com>
2022-05-30 08:47:11 +0700

    commit

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -1,20 +1,27 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai';
 
+import { Cart } from './';
+import { useStateContext } from '../context/StateContext';
+
 const Navbar = () => {
-  return (
-    <div className="navbar-container">
-        <p className='logo'>
-            <Link href="/">CXK Headphones</Link>
-        </p>
+    const { showCart, setShowCart, totalQuantities } = useStateContext();
 
-        <button type='button' className='cart-icon' onclick=''>
-            <AiOutlineShopping />
-            <span className='cart-item-qty'>1</span>
-        </button>
-    </div>
-  )
-}
+    return (
+        <div className='navbar-container'>
+            <p className='logo'>
+                <Link href='/'>CXK Headphones</Link>
+            </p>
 
-export default Navbar
+            <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
+                <AiOutlineShopping />
+                <span className='cart-item-qty'>{totalQuantities}</span>
+            </button>
+
+            {showCart && <Cart />}
+        </div>
+    );
+};
+
+export default Navbar;
