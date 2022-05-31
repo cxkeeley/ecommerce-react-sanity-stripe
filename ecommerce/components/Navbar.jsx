3d359f1969c93fd58548ddcddd3749ea002cf86e
cxkeeley <cxkeeley@gmail.com>
2022-05-31 09:52:44 +0700

    commit

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -6,22 +6,26 @@ import { Cart } from './';
 import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
-    const { showCart, setShowCart, totalQuantities } = useStateContext();
+  const { showCart, setShowCart, totalQuantities } = useStateContext();
 
-    return (
-        <div className='navbar-container'>
-            <p className='logo'>
-                <Link href='/'>CXK Headphones</Link>
-            </p>
+  return (
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>CXK Headphones</Link>
+      </p>
 
-            <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
-                <AiOutlineShopping />
-                <span className='cart-item-qty'>{totalQuantities}</span>
-            </button>
+      <button
+        type='button'
+        className='cart-icon'
+        onClick={() => setShowCart(true)}
+      >
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>{totalQuantities}</span>
+      </button>
 
-            {showCart && <Cart />}
-        </div>
-    );
+      {showCart && <Cart />}
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ ecommerce/pages/success.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link';
+
+const Success = () => {
+  return (
+    <div>success</div>
+  )
+}
+
+export default Success
